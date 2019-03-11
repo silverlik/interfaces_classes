999cf9753ed2aac4e809453b27956c2f1785a57a
Helena Sysoeva <alfeya@hotmail.com>
2019-03-11 14:45:22 -0400

    overriding/hiding

--- interfaces_classes/Program.cs
+++ interfaces_classes/Program.cs
@@ -5,11 +5,37 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace interfaces_classes
-{
+{      interface IUndo
+        {
+            void WriteName();
+        };
+    public class TextBox : IUndo
+    {
+        public virtual void  WriteName()
+        {
+            Console.WriteLine("TextBox: WriteName");
+        }
+        
+    };
+    public class RichTextBox : TextBox,IUndo
+    {
+        public  new void  WriteName()
+        {
+            Console.WriteLine("RichTextBox: WriteName");
+        }
+    };
     class Program
     {
+       
         static void Main(string[] args)
         {
+            RichTextBox r = new RichTextBox();
+            r.WriteName();
+            ((IUndo)r).WriteName();
+            ((TextBox)r).WriteName();
+            TextBox t = new TextBox();
+            t.WriteName();
+            Console.ReadKey();
         }
     }
 }
